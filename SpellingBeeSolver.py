d31bba017a85059928c19878559440ffb5eff056
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-11 18:52:30 +1000

    Add function to find solution using bitwise operations.
    Naive execution time:		0.060322087287902830 seconds
    
    Bitwise preprocessing time:	1.650294328689575000 seconds
    Bitwise execution time:		0.027350925445556642 seconds
    
    How do we define preprocessing? Technically you could preprocess the answer for every single combination of letters ever and store it in a massive dictionary. In which case execution time will become nothing. But I am choosing to find a "reasonable" middle ground. Preprocessing is any task which needs to be done only once no matter the input (to a reasonable extent).

--- SpellingBeeSolver.py
+++ SpellingBeeSolver.py
@@ -27,6 +27,87 @@ def get_bee_words_naive(center, others, dictionary):
 
     return valid_bee_words
 
+def preprocess_get_bit_to_word_dict(dictionary):
+    bit_dict = {}
+    for word in dictionary:
+        bits = ['0'] * 26
+        chars = set(word)
+        for c in chars:
+            bits[string.ascii_lowercase.index(c)] = "1"
+
+        bits = int(''.join(bits),2)
+        if bits in bit_dict:
+            bit_dict[bits].append(word)
+        else:
+            bit_dict[bits] = [word]
+
+    return bit_dict
+
+def preprocess_get_bit_to_negation_dict(dictionary):
+    negated_dict = {}
+    for word in dictionary:
+        bits = ['0'] * 26
+        negated = ['1'] * 26
+        chars = set(word)
+        for c in chars:
+            bits[string.ascii_lowercase.index(c)] = "1"
+            negated[string.ascii_lowercase.index(c)] = "0"
+
+        bits = int(''.join(bits),2)
+        negated = int(''.join(negated),2)
+        negated_dict[bits] = negated
+
+    return negated_dict
+
+def get_bee_words_bitwise(center, others, dictionary, word_to_negated_word):
+    check_bee_words_args(center, others)
+
+    """
+    truth table where word refers to the word we are checking. Center must always be present in the word.
+    Others should be optionally present in the word. We want the value to be greater than 0 if the word isnt valid.
+    
+        word    center  others  result  minterms
+        0       0       0       0
+        0       0       1       0
+        0       1       0       1       ((NOT word) AND center AND (NOT others))
+        0       1       1       0   # technically should never happen, so we put 0 to reduce our sum of products
+        1       0       0       1       (word AND (NOT center) AND (NOT others))    
+        1       0       1       0
+        1       1       0       0
+        1       1       1       0   # technically should never happen, so we put 0 to reduce our sum of products
+        
+        sop = ((NOT word) AND center AND (NOT others)) + (word AND (NOT center) AND (NOT others))
+            = (~W C ~O) + (W ~C ~O)
+        
+        This is why we calculate the negated words in advance, so that we don't have to waste time on it in this
+        function. Because NOT in python is weird. Should probably figured out how it works actually.
+    """
+
+    # We need to perform the bit operation [word OR (NOT letters)] to get the right answers
+    # However, bitwise not in python is weird because ints can be arbitrarily long
+    # Instead of figuring out how the not works, we just generate the NOT of letters and center
+    # in the first place. We have to do that work anyway, so this doesn't take extra time
+    # If anything, it takes less time.
+    other_bits_negated = ['1'] * 26
+    for o in others:
+        other_bits_negated[string.ascii_lowercase.index(o)] = "0"
+    other_bits_negated = int(''.join(other_bits_negated),2)
+    center_bits = ['0'] * 26
+    center_bits_negated = ['1'] * 26
+    center_bits[string.ascii_lowercase.index(center)] = "1"
+    center_bits_negated[string.ascii_lowercase.index(center)] = "0"
+    center_bits = int(''.join(center_bits),2)
+    center_bits_negated = int(''.join(center_bits_negated),2)
+
+    valid_bee_words = []
+    for word_bits in dictionary:
+        result = (word_to_negated_word[word_bits] & center_bits & other_bits_negated) | (word_bits & center_bits_negated & other_bits_negated)
+        if result == 0:
+            valid_bee_words.extend(dictionary[word_bits])
+
+    return valid_bee_words
+
+
 def pretty_print_solution(sol_list, center, others, uncommon = False):
     sol_list.sort()
 
@@ -58,11 +139,20 @@ def measure_execution_time(function, *args, iterations = 1):
 with open('dictionary/words_bee.txt') as f:
     words = f.read().splitlines()
 
+time_iters = 500
 # note: this combo seems to be the combo with the highest number of words
 # in the history of nyt spelling bee
 today_center = 'o'
 today_others = "ctpnme"
 # solution_words = get_bee_words_naive(today_center,today_others,words)
 # pretty_print_solution(solution_words,today_center,today_others)
+measure_execution_time(get_bee_words_naive,today_center,today_others,words, iterations = time_iters)
 
-measure_execution_time(get_bee_words_naive,today_center,today_others,words, iterations = 100)
+# bit_dictionary = preprocess_get_bit_to_word_dict(words)
+# negated_dictionary = preprocess_get_bit_to_negation_dict(words)
+# solution_words = get_bee_words_bitwise(today_center, today_others, bit_dictionary, negated_dictionary)
+# pretty_print_solution(solution_words, today_center, today_others)
+bit_dictionary = measure_execution_time(preprocess_get_bit_to_word_dict,words, iterations = time_iters)
+negated_dictionary = measure_execution_time(preprocess_get_bit_to_negation_dict,words, iterations = time_iters)
+solution_words = measure_execution_time(get_bee_words_bitwise,today_center, today_others, bit_dictionary, negated_dictionary, iterations = time_iters)
+# pretty_print_solution(solution_words, today_center, today_others)
