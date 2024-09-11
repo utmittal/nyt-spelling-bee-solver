05515718daa765f92c743d2dbf64e08eb4621a58
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-11 12:53:28 +1000

    Add naive solution.

--- SpellingBeeSolver.py
+++ SpellingBeeSolver.py
@@ -58,6 +58,30 @@ def analyze_dictionary(dictionary):
     print("\tWords with most single repeating letter (" + str(single_repeated_letter_count) + "):\n" + ('\n'.join([str("\t\t" + w) for w in words_with_most_single_repeated_letter[:10]])))
     print("\tWords with most total repeating letters (" + str(repeated_letters_count) + "):\n" + ('\n'.join([str("\t\t" + w) for w in words_with_most_repeated_letters[:10]])))
 
+def print_bee_words_naive(center, others, dictionary):
+    """
+    Naive approach. Simply iterate and check through whole dictionary.
+
+    :param center: Central character that must appear in word
+    :param others: List of other characters that must appear in word
+    :param dictionary: list of words to search in
+    :return: None
+    """
+    if len(others) > len(set(others)):
+        raise ValueError("List of other characters cannot contain repeated characters.")
+    if center in others:
+        raise ValueError("Central character cannot be in list of other characters.")
+
+    valid_bee_words = []
+    letter_set = set(others+center)
+    for word in dictionary:
+        if center in word:
+            if set(word) <= letter_set:
+                valid_bee_words.append(word)
+    valid_bee_words.sort()
+
+    print("Spelling Bee Solution - [" + center.upper() + " | " + ' '.join(c.upper() for c in others) + "] - " + str(len(valid_bee_words)) + " words:")
+    print('\n'.join([str("\t" + sol) for sol in valid_bee_words]))
 
 # with open('dictionary/words_alpha.txt') as f:
 #     words = f.read().splitlines()
@@ -65,4 +89,6 @@ def analyze_dictionary(dictionary):
 
 with open('dictionary/words_bee.txt') as f:
     words = f.read().splitlines()
-analyze_dictionary(words)
+# analyze_dictionary(words)
+
+print_bee_words_naive('e',"acfpty",words)
