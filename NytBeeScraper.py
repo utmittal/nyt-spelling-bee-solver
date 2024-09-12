e329fd11be40ab28a5f96aedcf4671c4bcda6a8d
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-12 13:48:05 +1000

    Add scraping file with full code to iterate through dates and hopefully protect against bad parsing.

--- NytBeeScraper.py
+++ NytBeeScraper.py
@@ -1,13 +1,56 @@
 from urllib.request import urlopen, Request
-
 from bs4 import BeautifulSoup
+import re
+from datetime import datetime, timedelta
 
-url = "https://nytbee.com/Bee_20240911.html"
-req = Request(url)
-req.add_header('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36')
-rawpage = urlopen(req).read()
+def get_raw_page(url):
+    req = Request(url)
 
-soup = BeautifulSoup(rawpage, "html.parser")
-answer_list = soup.find(id="main-answer-list")
+    user_agent_header = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+    req.add_header('user-agent',user_agent_header)
 
-print(answer_list.get_text(strip=True,separator=','))
+    return urlopen(req).read()
+
+def get_answer_list_from_nyt_page(raw_web_page):
+    soup = BeautifulSoup(raw_web_page, "html.parser")
+
+    # The list of answers seems to always be in a div with id=main-answer-list
+    answer_list_div = soup.find(id="main-answer-list")
+    if answer_list_div is None:
+        raise LookupError("Could not find element with id=main-answer-list.")
+
+    # all_text should contain answers and ↗ (the symbol for the definition link on the nytbee page)
+    all_text = answer_list_div.get_text(strip=True, separator=',')
+    answers = [w for w in all_text.split(sep=',') if w.isalpha()]
+    if len(answers) == 0:
+        raise LookupError("Found answer list of length 0.")
+
+    # Verify our answer list
+    number_of_answers = re.search(">Number of Answers: \(+\\d\)",raw_web_page)
+    groups = number_of_answers.groups()
+    if len(groups) > 1:
+        raise LookupError("Found more than one match for number of answers.")
+    else:
+        web_page_answers_count = int(groups[0])
+        if web_page_answers_count != len(answers):
+            raise LookupError("Number of answers as per page is " + str(web_page_answers_count) + " but the scraper only found " + str(len(answers)) + " answers.")
+
+    return answers
+
+def write_words_to_dictionary(word_list):
+    # Note: we specifically open in append mode so that if for some reason the file doesn't exist, the program fails.
+    # I want to know if the file is gone (because something has gone wrong)
+    with open('dictionary/nytbee_dot_com_scraped_answers.txt','a') as writefile:
+        writefile.writelines(word + '\n' for word in word_list)
+
+while True:
+    date_object = datetime(year=2024,month=9,day=12)
+    date_object = date_object - timedelta(days=1)
+    date_string = date_object.strftime('%Y%m%d')
+    current_url = "https://nytbee.com/Bee_"+date_string+".html"
+    print("Processing - " + current_url)
+    raw_page = get_raw_page(current_url)
+    answer_list = get_answer_list_from_nyt_page(raw_page)
+    print("Found " + str(len(answer_list)) + " words.")
+    # Note: we don't dedupe the answers before writing. We just want all the answers.
+    write_words_to_dictionary(answer_list)
