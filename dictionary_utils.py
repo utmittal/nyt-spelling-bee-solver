9f8646b20f4ba314253e308dc42c7a1922fdbe18
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-18 19:35:30 +1000

    Add function to return path to largest dictionary for benchmarking.

--- dictionary_utils.py
+++ dictionary_utils.py
@@ -5,3 +5,8 @@ def get_latest_custom_dictionary_path():
     dics = os.listdir('dictionaries/custom')
     dics.sort(reverse=True)
     return 'dictionaries/custom/' + dics[0]
+
+
+def get_benchmarking_dictionary_path():
+    # Use biggest dictionary for benchmarking
+    return 'dictionaries/raw/words_alpha.txt'
