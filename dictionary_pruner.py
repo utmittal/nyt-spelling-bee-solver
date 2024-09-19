39da77dbc300f31fcde2b8e0f44fb862c0eea4a3
Utkarsh Mittal <mittal.utk@gmail.com>
2024-09-19 17:04:17 +1000

    Add script to prune dictionary based on past NYT answers.
    This script parses the raw answers from the nytbee.com scrape and tries to identify valid puzzles. For every puzzle it identifies, it runs our own solver and compares it against the nytbee.com list to find words to add/delete.
    
    We could just scrape nytbee.com again but I would rather not. This is good enough.

---
+++ dictionary_pruner.py
@@ -0,0 +1,73 @@
+"""
+Experimental code to prune our custom dictionary based on past correct answers from nytbee.com.
+
+To avoid scraping the website again, I am going to try to read the existing file and try to identify the puzzle based
+on the answers. And then get answers from our dictionary for that puzzle and add/remove words as required.
+"""
+from dictionary_utils import PROJECT_ROOT_PATH, get_latest_custom_dictionary
+from interactive_editor import add_new_words, delete_words
+from spelling_bee_solvers import preprocess_get_radix_tree, get_bee_solutions_radix_tree
+
+radix_tree = preprocess_get_radix_tree(get_latest_custom_dictionary(), {})
+
+
+def _get_bee_sols(cen: str, oth: list[str]) -> list[str]:
+    return get_bee_solutions_radix_tree(cen, ''.join(oth), radix_tree)
+
+
+with open(f'{PROJECT_ROOT_PATH}/dictionaries/raw/nytbee_dot_com_scraped_answers.txt', 'r') as reader:
+    nyt_bee_answers = reader.read().splitlines()
+
+current_letters = set()
+current_words = set()
+probable_center = set()
+total_add = []
+total_delete = []
+for word in nyt_bee_answers:
+    letter_set = set(word)
+
+    if len(current_letters.union(letter_set)) > 7:
+        if len(current_letters) == 7:
+            if len(probable_center) != 1:
+                # Add all words but don't delete anything
+                total_add.extend(current_words)
+                print(f"Could not find center for letter set: {current_letters}.")
+                current_letters = set()
+                current_words = set()
+                probable_center = set()
+                continue
+
+            center = probable_center.pop()
+            current_letters.remove(center)
+            our_answers = set(_get_bee_sols(center, list(current_letters)))
+
+            words_to_delete = our_answers - current_words
+            total_delete.extend(words_to_delete)
+            words_to_add = current_words - our_answers
+            total_add.extend(words_to_add)
+            # print(f"center: {center}")
+            # print(f"others: {current_letters}")
+            # print(f"nyt bee words: {current_words}")
+            # print(f"our answers: {our_answers}")
+            # print(f"delete: {words_to_delete}")
+            # print(f"add: {words_to_add}")
+
+            current_letters = set()
+            current_words = set()
+            probable_center = set()
+        else:
+            raise Exception(
+                "Current word seems to start a new puzzle but we haven't found all the letters in the old puzzle yet.")
+    else:
+        current_letters.update(letter_set)
+        if len(probable_center) == 0:
+            probable_center.update(letter_set)
+        else:
+            probable_center.intersection_update(letter_set)
+        current_words.add(word)
+
+print(total_add)
+print(total_delete)
+
+add_new_words(total_add)
+delete_words(total_add)
