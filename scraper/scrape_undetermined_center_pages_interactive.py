66c158b5ccbf034710152b9ee334842d3efa6751
Utkarsh Mittal <mittal.utk@gmail.com>
2024-10-03 12:45:08 +1000

    Update undetermined scrape script to handle json file as well as to write the changes to scraped_urls.

--- scraper/scrape_undetermined_center_pages_interactive.py
+++ scraper/scrape_undetermined_center_pages_interactive.py
@@ -1,11 +1,16 @@
+from datetime import datetime
+
 from data.dictionary_utils import get_custom_dictionary, get_dictionary_from_path, \
     write_words_to_dictionary, add_words_to_custom, delete_words_from_custom
+from data.puzzles_utils import get_puzzles_from_file, write_puzzles_to_file, NYTBeePuzzle
 from scraper.nyt_bee_scraper import get_url_date_dict_from_logfile, get_url_from_date, get_raw_page, \
     get_answer_list_from_nyt_page, write_url_date_dict_to_logfile
 from spelling_bee_solvers import get_bee_solutions_radix_tree, preprocess_get_radix_tree
 
+scraped_urls = get_url_date_dict_from_logfile('scraper/logs/scraped_dates.txt')
 undetermined_center_urls = get_url_date_dict_from_logfile('scraper/logs/undetermined_center_pages.txt')
-unique_words = set(get_dictionary_from_path('data/raw_word_lists/nytbee_dot_com_scraped_answers.txt'))
+unique_words = set(get_dictionary_from_path('data/processed/nytbee_dot_com_scraped_answers.txt'))
+scraped_puzzles = get_puzzles_from_file()
 
 radix_tree = preprocess_get_radix_tree(get_custom_dictionary(), {})
 
@@ -15,6 +20,7 @@ determined_urls = set()
 for current_url in undetermined_center_urls:
     print("Processing - " + current_url)
     raw_page = get_raw_page(current_url)
+    date_object = datetime.strptime(undetermined_center_urls[current_url], '%Y%m%d').date()
 
     answer_list = get_answer_list_from_nyt_page(raw_page)
     print("Found " + str(len(answer_list)) + " words.")
@@ -53,14 +59,19 @@ for current_url in undetermined_center_urls:
         print(f"\t{new_words} to be added.")
         words_to_add.update(new_words)
 
+        scraped_puzzles[date_object] = NYTBeePuzzle(date_object, user_entered_center, todays_letter_set, answer_list)
+
     unique_words.update(answer_list)
 
     determined_urls.add(current_url)
 
 for url in determined_urls:
+    scraped_urls[url] = undetermined_center_urls[url]
     undetermined_center_urls.pop(url)
 
 write_url_date_dict_to_logfile(undetermined_center_urls, 'scraper/logs/undetermined_center_pages.txt')
-write_words_to_dictionary(unique_words, 'data/raw_word_lists/nytbee_dot_com_scraped_answers.txt')
+write_words_to_dictionary(unique_words, 'data/processed/nytbee_dot_com_scraped_answers.txt')
+write_puzzles_to_file([scraped_puzzles[p] for p in scraped_puzzles])
+write_url_date_dict_to_logfile(scraped_urls, 'scraper/logs/scraped_dates.txt')
 add_words_to_custom(words_to_add)
 delete_words_from_custom(words_to_delete)
